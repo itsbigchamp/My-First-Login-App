de0f64b1924191495b9cde4e4c7a787f1ea6e6d7
Gavin Anderson <btcgrowth1@gmail.com>
2022-08-11 13:44:17 -0400

    added google login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react"
 import { initializeApp } from "firebase/app"
-import { createUserWithEmailAndPassword, getAuth} from "firebase/auth"
+import { 
+    createUserWithEmailAndPassword, getAuth, 
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signInWithPopup} from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyDRuHnFSILPHet02P3kmf55tpa9bVLwSwI",
@@ -15,11 +19,35 @@ const firebaseConfig = {
 function Login({ setIsLoggedIn }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleSignUp = async () => {
+    const connectAuth = async () => {
         // connect to firebase
         const app = initializeApp(firebaseConfig);
         // connect to auth
-        const auth = getAuth(app);
+        return getAuth(app);
+
+    }
+    const handleLogin = async () => {
+        const auth = await connectAuth()
+        const user = await signInWithEmailAndPassword(auth, email, password)
+            .catch(err => alert(err.message))
+        if(user) {
+            console.log(user.user)
+            setIsLoggedIn(true)
+        }
+    }
+
+    const handleGoogleLogin = async () => {
+        const auth = await connectAuth()
+        const provider = new GoogleAuthProvider()
+        const user = await signInWithPopup(auth, provider)
+            .catch(err => alert(err.message))
+        if(user) {
+            console.log(user.user)
+            setIsLoggedIn(true)
+        }
+    }
+    const handleSignUp = async () => {
+        const auth = await connectAuth()
         // send email and password to firebase auth
         const user = await createUserWithEmailAndPassword(auth, email, password)
             .catch(err => alert(err.message))
@@ -36,13 +64,16 @@ function Login({ setIsLoggedIn }) {
                 value={email} onChange={e => setEmail(e.target.value)}
                 name="email" type="email" placeholder="you@there.com"/>
             </label><br/>
-            <label for="password">
+            <label htmlFor="password">
                 Password:
                 <input
                 value={password} onChange={e => setPassword(e.target.value)}
                 name="password" type="password" placeholder="*****"/>
             </label><br/>
+            <button onClick={handleLogin}>Login</button>&nbsp;
             <button onClick={handleSignUp}>Sign Up</button>
+            <br />
+            <button onClick={handleGoogleLogin}>Login with Google</button>
         </form>
     )
 
